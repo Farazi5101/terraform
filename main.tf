bdc144b60255ea853a4a831cc0d880f3da3d7392
Farazi5101 <farazidastagir5109@gmail.com>
2023-06-20 15:33:37 +0530

    uploading1

--- main.tf
+++ main.tf
@@ -1,166 +1,36 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.3.0"
-    }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_launch_configuration" "as_conf" {
+  name_prefix   = "terraform-lc-example-"
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-provider "aws" {
-  
-}
+resource "aws_autoscaling_group" "bar" {
+  name                 = "terraform-asg-example"
+  launch_configuration = aws_launch_configuration.as_conf.name
+  min_size             = 1
+  max_size             = 2
 
-
-resource "aws_kinesis_firehose_delivery_stream" "extended_s3_stream" {
-  name        = "terraform-kinesis-firehose-extended-s3-test-stream"
-  destination = "extended_s3"
-
-  extended_s3_configuration {
-    role_arn   = aws_iam_role.firehose_role.arn
-    bucket_arn = aws_s3_bucket.bucket.arn
-
-    processing_configuration {
-      enabled = "true"
-
-      processors {
-        type = "Lambda"
-
-        parameters {
-          parameter_name  = "LambdaArn"
-          parameter_value = "${aws_lambda_function.lambda_processor.arn}:$LATEST"
-        }
-      }
-    }
+  lifecycle {
+    create_before_destroy = true
   }
-}
-
-resource "aws_s3_bucket" "bucket" {
-  bucket = "tf-test-bucket"
-}
-
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.bucket.id
-  acl    = "private"
-}
-
-data "aws_iam_policy_document" "firehose_assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["firehose.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "firehose_role" {
-  name               = "firehose_test_role"
-  assume_role_policy = data.aws_iam_policy_document.firehose_assume_role.json
-}
-
-data "aws_iam_policy_document" "lambda_assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "lambda_iam" {
-  name               = "lambda_iam"
-  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
-}
-
-resource "aws_lambda_function" "lambda_processor" {
-  filename      = "lambda.zip"
-  function_name = "firehose_lambda_processor"
-  role          = aws_iam_role.lambda_iam.arn
-  handler       = "exports.handler"
-  runtime       = "nodejs16.x"
-}
-
-resource "aws_wafv2_web_acl" "example" {
-  name        = "managed-rule-example"
-  description = "Example of a managed rule."
-  scope       = "REGIONAL"
-
-  default_action {
-    allow {}
-  }
-
-  rule {
-    name     = "rule-1"
-    priority = 1
-
-    override_action {
-      count {}
-    }
-
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesCommonRuleSet"
-        vendor_name = "AWS"
-
-        rule_action_override {
-          action_to_use {
-            count {}
-          }
-
-          name = "SizeRestrictions_QUERYSTRING"
-        }
-
-        rule_action_override {
-          action_to_use {
-            count {}
-          }
-
-          name = "NoUserAgent_HEADER"
-        }
-
-        scope_down_statement {
-          geo_match_statement {
-            country_codes = ["US", "NL"]
-          }
-        }
-      }
-    }
-
-    
-
-    visibility_config {
-      cloudwatch_metrics_enabled = false
-      metric_name                = "friendly-rule-metric-name"
-      sampled_requests_enabled   = false
-    }
-  }
-
-  tags = {
-    Tag1 = "Value1"
-    Tag2 = "Value2"
-  }
-
-  visibility_config {
-    cloudwatch_metrics_enabled = false
-    metric_name                = "friendly-metric-name"
-    sampled_requests_enabled   = false
-  }
-}
-
-resource "aws_wafv2_web_acl_logging_configuration" "example" {
-  log_destination_configs = [aws_kinesis_firehose_delivery_stream.extended_s3_stream.arn]
-  resource_arn            = aws_wafv2_web_acl.example.arn
-  //redacted_fields {
-   // single_header {
-      //name = "randomvalue"
-   // }
- // }
 }
